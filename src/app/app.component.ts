bcd7501f7a0e74b5e1e1543394932ec7903b618f
Stepalom77 <stepalom22@gmail.com>
2022-11-06 18:08:14 -0500

    Assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username:string = ''
   name = 'my first Angular App';
+
+  updateUsername(){
+    this.username = ''
+  }
 }
